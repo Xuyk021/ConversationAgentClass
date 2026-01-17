8626460f3e547bcd69623491a89d021db6e48533
Xuyk021 <1455178537@qq.com>
2026-01-17 16:25:28 -0500

    init class 1 python

---
+++ Class1-Hello.py
@@ -0,0 +1,11 @@
+# Import the Streamlit library for building web applications
+import streamlit as st
+
+# Set the configuration for the Streamlit page, including the title
+st.set_page_config(page_title="Echo Agent")
+
+# Display the main title of the web application
+st.title("Echo Agent")
+
+# Write a simple message to the web application
+st.write("Hello Echo Agent!")
